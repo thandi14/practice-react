d06ff76a14da0778a0da5bcad7de960dbe7684d2
thandi mpofu <thandimpofu2003@gmail.com>
2023-06-20 15:54:39 -0500

    practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Showcase from "./Showcase";
+
+
 function App() {
   return (
     <div>
-      <h1>App Component</h1>
+      <Showcase />
+
     </div>
   );
 }
---
+++ src/Showcase.js
@@ -0,0 +1,9 @@
+function Showcase() {
+    return (
+      <div>
+        <h1>Showcase Component</h1>
+      </div>
+    );
+  }
+
+  export default Showcase;
